2975426eee044bddbdba627589f6f77879bc2716
jochen <jochen.francois@kbc.be>
2020-09-04 21:08:37 +0200

    Setup Router component

--- qover-frontend/src/App.tsx
+++ qover-frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+
+import Login from './pages/Login/Login.lazy';
+import Configurator from './pages/Configurator/Configurator.lazy';
+import PlanOverview from './pages/PlanOverview/PlanOverview.lazy';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path="/configurator">
+          <Configurator />
+        </Route>
+        <Route path="/planOverview">
+          <PlanOverview />
+        </Route>
+      </Switch>
     </div>
   );
 }
