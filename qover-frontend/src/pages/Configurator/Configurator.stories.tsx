c4aa5c28616c6c750e36e5a85fa32126e11f6011
jochen <jochen.francois@kbc.be>
2020-09-04 21:08:04 +0200

    Add Configurator Page (draft)

---
+++ qover-frontend/src/pages/Configurator/Configurator.stories.tsx
@@ -0,0 +1,6 @@
+/* eslint-disable */
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Configurator from './Configurator';
+
+storiesOf('Configurator', module).add('default', () => <Configurator />);
---
+++ qover-frontend/src/pages/Configurator/Configurator.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Configurator.module.css';
+
+const Configurator: React.FC = () => (
+  <div className={styles.Configurator}>
+    Configurator Component
+  </div>
+);
+
+export default Configurator;
