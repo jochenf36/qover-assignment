09a72e5dd1c6d3fbb79bf86f0e5cce96f2544569
jochen <jochen.francois@kbc.be>
2020-09-07 14:16:28 +0200

    Remove unused controller

--- qover-backend/src/app.module.ts
+++ qover-backend/src/app.module.ts
@@ -3,10 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [AuthModule, UsersModule, CarModule],
+  imports: [AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- qover-backend/src/car/car.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CarController } from './car.controller';
-import { AuthModule } from 'src/auth/auth.module';
-
-@Module({ imports: [AuthModule], controllers: [CarController] })
-export class CarModule {}
